947848c04acfe55941c508267b8afba5c40948f5
scott-oslund <sroslund@ucsc.edu>
2023-01-18 01:31:39 -0800

    added alternate API

---
+++ DataCollector/pushapi.py
@@ -0,0 +1,8 @@
+import datetime as dt
+import praw
+from pmaw import PushshiftAPI
+
+agent = "r/ucsc data scrapper"
+reddit = praw.Reddit(client_id="edhlsn3xRnsuK_U1n_qOfQ", client_secret="DyZHk39I6aJmKweONoS3IFU2kmyHIQ", user_agent = agent)
+
+api = PushshiftAPI(reddit)
--- DataCollector/scrapper.py
+++ DataCollector/scrapper.py
@@ -1,6 +1,7 @@
 import numpy as np
 import praw 
 import textwrap
+from pmaw import PushshiftAPI
 
 agent = "r/ucsc data scrapper"
 reddit = praw.Reddit(client_id="edhlsn3xRnsuK_U1n_qOfQ", client_secret="DyZHk39I6aJmKweONoS3IFU2kmyHIQ", user_agent = agent)
@@ -12,6 +13,7 @@ def show_replies(comment, depth=0):
 
 for submission in reddit.subreddit('ucsc').top(limit=2):
     print('#####################################')
+    submission.comments.replace_more(limit=None)
     print(submission.title)
     for comment in submission.comments:
         show_replies(comment)
